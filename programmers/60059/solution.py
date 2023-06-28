b394f12d6e56bb44974dbc2f9cb0a611be3cb5fc
MANY <anjffhgkwl97@gmail.com>
2023-06-28 19:22:06 +0900

    2023_06_28 success

--- programmers/60059/solution.py
+++ programmers/60059/solution.py
@@ -4,29 +4,19 @@ def turn(arr):
         for j in range(len(arr)):
             t[j][-1-i] = arr[i][j]
     return t
+
 def check(arr, answer):
-    c = []
-    for i in range(len(answer)):
-        c.append([])
-        for j in range(len(answer)):
-            c[-1].append(arr[i][j] + answer[i][j])
-    for i in c:
-        for j in i:
-            if j != 1:
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            c = arr[i][j] + answer[i][j]
+            if c == 2 or c==0:
                 return False
-    return True    
+    return True
 
 def solution(key, lock):
-    answer = [(i,j) for i in range(len(lock)) for j in range(len(lock)) if lock[i][j] == 0]
+    answer = [ (i,j) for i in range(len(lock)) for j in range(len(lock)) if lock[i][j] == 0]
     xs, xe = len(lock), 0
     ys, ye = len(lock), 0
-
-
-    print('lock')
-    for k in lock:
-        print(k)
-    print()
-
     for y, x in answer:
         if xs > x:
             xs = x
@@ -36,23 +26,14 @@ def solution(key, lock):
             ys = y
         if ye <= y:
             ye = y+1
+    
     answer = []
     for i in range(ys, ye):
         answer.append([])
         for j in range(xs, xe):
             answer[-1].append(lock[i][j])
-    
-    print('answer')
-    for k in answer:
-        print(k)
-    print()
-    
+
     for i in range(4):
-        print('key')
-        for k in key:
-            print(k)
-        print()
-        
         for y in range(len(key)-(ye-ys)+1):
             for x in range(len(key)-(xe-xs)+1):
                 t = []
@@ -60,20 +41,7 @@ def solution(key, lock):
                     t.append([])
                     for j in range(x, x+(xe-xs)):
                         t[-1].append(key[i][j])
-                
-                print('부분')
-                for k in t:
-                    print(k)
-                print()
-                    
                 if check(t, answer):
                     return True
         key = turn(key)
     return False
-
-
-key = [[1,0,0], [1,1,0], [0,0,0]]
-lock = [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
-print(solution(key, lock))
-
-# 3 7 11 20 24 26 28 30 33 36 37
