cc636d81397532e662d1243c63e5cf349340bcd4
MANY <anjffhgkwl97@gmail.com>
2023-07-04 15:14:06 +0900

    2023_07_04

--- programmers/42892/solution.py
+++ programmers/42892/solution.py
@@ -1,35 +1,33 @@
-from collections import defaultdict, deque
+from collections import deque
+import sys
+sys.setrecursionlimit(10**6)
 
 nodeinfo = [[5,3],[11,5],[13,3],[3,5],[6,1],[1,3],[8,6],[7,2],[2,2]]
 
-
 arr = list(zip(range(len(nodeinfo)),nodeinfo))
 arr = deque(sorted(arr, key=lambda x:(x[1][1], -x[1][0])))
-print(arr)
-
-v = []
-def preorder(arr:deque, node):
-    if not arr:
+def preorder(arr:deque, pre, post):
+    if len(arr) < 1:
         return
-    
-    t = arr.pop()
-    if t[1][1] == node[1][1]:
-        arr.appendleft(t)
+    n = arr.pop()
+    l = deque([])
+    r = deque([])
+    while arr:
         t = arr.pop()
+        if t[1][0] < n[1][0]:
+            l.appendleft(t)
+        else:
+            r.appendleft(t)
+    pre.append(n[0]+1)
+    preorder(l, pre, post)
+    preorder(r, pre, post)
+    post.append(n[0]+1)
+    
+pre, post = [], []
+preorder(arr,pre, post)
+print(pre)
+print(post)
 
-    if t[1][0] > node[1][0] or not arr:
-        v.append(t)
-        preorder(arr, t)
-    else:
-        r = arr.pop()
-        v.append(t)
-        preorder(arr, t)
-        v.append(r)
-        preorder(arr, r)
-
-n = arr.pop()
-preorder(arr, n)
-print(v)
 
     
     
