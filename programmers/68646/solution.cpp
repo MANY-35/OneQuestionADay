a72178d8ae6465599232d57963755baab7e0f3dc
MANY <anjffhgkwl97@gmail.com>
2023-10-19 16:03:54 +0900

    2023_10_19

---
+++ programmers/68646/solution.cpp
@@ -0,0 +1,45 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int min(vector<int> &arr, int end) {
+    int m = 0;
+    for(int i=0; i<end; i++) {
+        if (arr[i] < arr[m])
+            m = i;
+    }
+    return m;
+}
+int solution(vector<int> a) {
+    int answer = 1;
+    int lm, rm;
+    lm = rm = 1000000000;
+    int index = min(a, a.size());
+
+    for(int i=0; i<index; i++) {
+        cout << a[i] << " ";
+        if (a[i] < lm) {
+            lm = a[i];
+            answer ++;
+        }
+    }
+    for(int i=a.size()-1; i>index; i--) {
+        if(a[i] < rm) {
+            rm = a[i];
+            answer ++;
+        }
+    }
+    return answer;
+}
+int main(void)
+{
+    vector<int> a;
+    a = {
+       -16
+    };
+    
+    cout << solution(a);
+    return 0;
+}
