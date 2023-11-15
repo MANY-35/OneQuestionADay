e19418e48224fca869f8f8657d62a98230a6ef7d
MANY <anjffhgkwl97@gmail.com>
2023-11-15 18:40:46 +0900

    2023_11_15

--- programmers/77886/solution.cpp
+++ programmers/77886/solution.cpp
@@ -8,37 +8,38 @@ using namespace std;
 
 vector<string> solution(vector<string> s) {
     vector<string> answer;
-    
     for (string str : s) {
         vector<char> stack = {str[0]};
-        int count = 0;
-        char last = str[1];
-        for(int i=2; i<str.size(); i++) {
-            if(str[i] == '0' && last == '1' && stack.back() == '1') {
-                count++;
-                stack.pop_back();
-                last = stack.back();
-                stack.pop_back();
-            }
+        int count = 0, s_i =0;
+        for (int i=1; i<str.size(); i++) {
+            if(s_i < 1)
+                stack.push_back(str[i]);
             else {
-                stack.push_back(last);
-                last = str[i];
+            
+                if (stack[s_i-1] == '1' && stack[s_i] == '1' && str[i] == '0') {
+                    count++;
+                    stack.pop_back();
+                    stack.pop_back();
+                    s_i -= 2;
+                } else {
+                    stack.push_back(str[i]);
+                    s_i++;
+                }
+  
             }
         }
-        stack.push_back(last);
-        int i;
-        for(i=stack.size()-1; i>=0; i--) {
-            if (stack[i] == '0')
+        int index;
+        for(index=stack.size()-1; index>=0; index--) {
+            if (stack[index] == '0')
                 break;
         }
-        string left(stack.begin(), stack.begin()+i+1);
-        string right(stack.begin()+i+1, stack.end());
+        string left(stack.begin(), stack.begin()+index+1);
+        string right(stack.begin()+index+1, stack.end());
         for(int j=0; j<count; j++)
             left += "110";
         left += right;
         answer.push_back(left);
-
-        cout << left << endl;
+        cout << left;
     }
 
     return answer;
@@ -46,7 +47,7 @@ vector<string> solution(vector<string> s) {
 
 int main(void)
 {
-    vector<string> s = {"0100110"};
+    vector<string> s = {"1110"};
     // srand(time(NULL));
     // string str = "";
     // int k = rand();
@@ -60,6 +61,7 @@ int main(void)
     // }
     // cout << str << endl;
     // s.push_back(str);
+
 
     solution(s);
 }
