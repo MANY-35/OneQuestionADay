53127a981e9fe5d4a00ef8d00a7181798ddea78e
MANY <anjffhgkwl97@gmail.com>
2023-11-22 19:23:52 +0900

    2023_11_22

--- programmers/70130/solution.cpp
+++ programmers/70130/solution.cpp
@@ -4,64 +4,60 @@
 #include <map>
 using namespace std;
 
-void permutation(map<vector<int>, int> &per, vector<int> &arr, vector<int> t, int c, int n) {
-    if(n-t.size() > arr.size()-c)
-        return;
-    if(t.size() == n) {
-        per[t] = 0;
-        return;
-    }
-    for(int i=c; i<arr.size(); i++) {
-        t.push_back(arr[i]);
-        permutation(per, arr, t, i+1, n);
-        t.pop_back();
-    }
-}
-
-bool checkStar(vector<int> arr) {
-    map<int, int> t;
-    int max = 0;
-    for(int i=0; i<arr.size(); i+=2) {
-        if(arr[i] == arr[i+1])
-            return false;
-        for (int j=0; j<2; j++) {
-            t[arr[i+j]]++;
-            if(max < t[arr[i+j]])
-                max = t[arr[i+j]];
-        }
-    }
-    if (arr.size()/2 == max)
-        return true;
-    return false;
-}
-
 int solution(vector<int> a) {
-    if (a.size() < 2)
+    int len = a.size();
+    if (len < 2)
         return 0;
-    else if(a.size() == 2) {
+    else if(len == 2) {
         if(a[0] == a[1])
             return 0;
         else
             return 2;
     }
+    map<int, vector<int>> numbers;
+    for(int i=0; i<len; i++)
+        numbers[a[i]].push_back(i);
 
-    int i = 2;
-    while(i+2 <= a.size())
-        i+=2;
+    if (numbers.size() < 2)
+        return 0;
 
-    for(; i > 2; i-=2) {
-        map<vector<int>, int> p = {};
-        permutation(p, a, {}, 0, i);
-        for(auto k : p) {
-            if (checkStar(k.first))
-                return i;
+    int answer = 0;
+    for(auto number : numbers) {
+        number.second.push_back(len);
+
+        cout << number.first << endl;
+        for(int i=0; i<number.second.size(); i++) 
+            cout << number.second[i] << " ";
+        cout << endl;
+        
+        int sum = 0;
+        if(number.second[0] - 0 > 0)
+            sum++;
+        else if(number.second[1] - number.second[0] - 1 > 0) {
+            sum++;
+            number.second[0]++;
         }
+        for(int i=1; i<number.second.size()-1; i++) {
+            if(number.second[i] - number.second[i-1] - 1 > 0)
+                sum++;
+            else if(number.second[i+1] - number.second[i] - 1 > 0) {
+                sum++;
+                number.second[i]++;
+            }   
+        }
+        for(int i=0; i<number.second.size(); i++) 
+            cout << number.second[i] << " ";
+        if (answer < sum)
+            answer = sum;
+        cout << sum << endl;
+        cout << endl;
     }
-    return 0;
+
+    return answer*2;
 }
 
 int main(void) {
-    vector<int> a = {0, 3, 0, 3, 3, 2, 3, 2, 2, 0, 0, 0, 0, 0, 0, 3, 0, 0, 3};
+    vector<int> a = {5, 5, 5, 6};
     cout << solution(a) << endl;
     
 }   
