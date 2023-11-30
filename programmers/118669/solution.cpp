db2811a0625eb2224772d621cc2270f74f412ad4
MANY <anjffhgkwl97@gmail.com>
2023-11-30 19:53:28 +0900

    2023_11_30

--- programmers/118669/solution.cpp
+++ programmers/118669/solution.cpp
@@ -3,76 +3,106 @@
 #include <iostream>
 #include <utility>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
-#define my_priority_data pair<int, int>, vector<pair<int, int>>, cmp
 
 struct cmp {
-    bool operator()(pair<int, int>a, pair<int, int>b) {
-        return a.second > b.second;
+    bool operator()(vector<int> a, vector<int> b) {
+        if (a[2] == b[2])
+            return a[1] > b[1];
+        return a[2] > b[2];
     }
 };
-
-int func(vector<priority_queue<my_priority_data>> &arr, int n, int e, int m, priority_queue<my_priority_data> que, bool vist[]) {
-    if (n == e)
-        return m;
-
-    if (vist[n])
-        return 0;
-
-    vist[n] = true;
+int func(vector<vector<int>> &arr, priority_queue<vector<int>, vector<vector<int>>, cmp> &que, vector<int> &summits, vector<int> &link) {
     while(!que.empty()) {
-        pair<int, int> p = que.top();
-        cout << p.first << " " << m  << " " << p.second << endl;
-        if(func(arr, p.first, e, max(m, p.second), arr[p.first], vist))
-            return max(m, p.second);
-        vist[p.first] = false;
+        vector<int> top = que.top();
         que.pop();
+
+        link[top[1]] = top[0];
+        for(int summit : summits) {
+            if (top[1] == summit)
+                return summit;
+        }
+        
+        for(int i=1; i<arr[top[1]].size(); i++) {
+            if(!arr[top[1]][i])
+                continue;
+
+            if(!link[i])
+                que.push({top[1], i, arr[top[1]][i]});
+        }
     }
-    return 0;
+    return -1;
 }
 
 vector<int> solution(int n, vector<vector<int>> paths, vector<int> gates, vector<int> summits) {
-    vector<priority_queue<my_priority_data>> arr(n+1);
+    vector<vector<int>> arr(n+1, vector<int>(n+1, 0));
     for (vector<int> path : paths) {
-        arr[path[0]].push(make_pair(path[1], path[2]));
-        arr[path[1]].push(make_pair(path[0], path[2]));
+        arr[path[0]][path[1]] = path[2];
+        arr[path[1]][path[0]] = path[2];
     }
-    
-    vector<int> m = {0, 10000000};
-    for (int summit : summits) {
-        for(int gate : gates) {
-            bool *vist = new bool[n+1];
-            fill_n(vist, n+1, false);
-            int k = func(arr, gate, summit, 0, arr[gate], vist);
-            cout << k << endl;
-            if(m[1] > k) {
-                m[0] = summit;
-                m[1] = k;
-            }
+
+    for(int gate : gates) {
+        for(int i=0; i<arr.size(); i++) {
+            arr[i][gate] = 0;
         }
     }
-    return m;
+    for(int summit : summits)
+        arr[summit] = vector<int>(n+1, 0);
+
+    for(auto i : arr) {
+        for (auto j : i)
+            cout << j << " ";
+        cout << endl;
+    }   
+
+    sort(summits.begin(), summits.end());
+
+    vector<int> link(n+1, 0);
+    priority_queue<vector<int>, vector<vector<int>>, cmp> que;
+    for(int gate : gates) {
+        link[gate] = -1;
+        for(int i=1; i<arr[gate].size(); i++) {
+            if(arr[gate][i] != 0)
+                que.push({gate, i, arr[gate][i]});
+        }
+    }
+    
+    vector<int> answer;
+    int l = func(arr, que, summits, link);
+    int r = 0;
+    answer.push_back(l);
+    while(link[l] != -1) {
+        if(r < arr[link[l]][l])
+            r = arr[link[l]][l];
+        l = link[l];
+    }
+    answer.push_back(r);
+    cout << answer[0] << " " << answer[1] << endl;
+
+    return answer;
 }
 
 int main(void) {
     int n = 7;
 
     vector<vector<int>> paths = {
-        {1, 4, 4},
         {1, 6, 1},
-        {1, 7, 3},
-        {2, 5, 2},
-        {3, 7, 4},
-        {5, 6, 6},
+        {1, 4, 1},
+        {6, 7, 1},
+        {6, 2, 1},
+        {4, 5, 1},
+        {5, 2, 1},
+        {2, 3, 1}
     };
 
     vector<int> gates = {
-        {1}
+        {3, 7}
     };
 
     vector<int> summits = {
-        {2,3,4}
+        {1, 5}
     };
     
     auto answer = solution(n, paths, gates, summits);
