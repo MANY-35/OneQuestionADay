7f7d82f6ebed4e44ccc1d52694cb51722432b116
MANY <anjffhgkwl97@gmail.com>
2024-09-06 13:07:53 +0900

    2024_09_06

---
+++ programmers/72410/solution.py
@@ -0,0 +1,36 @@
+def solution(new_id : str):
+    task = ''
+    for c in new_id:
+        if c.isalpha() :
+            task += c.lower()
+        elif c.isdigit() :
+            task += c
+        elif c in ["-", "_", "."] :
+            task += c
+    flag = True
+    answer = ""
+    for c in task:
+        if c == '.':
+            if flag:
+                flag = False
+                answer += c
+        else:
+            answer += c 
+            flag = True
+    if answer.startswith('.'):
+        answer = answer[1:]
+    if answer.endswith('.'):
+        answer = answer[:-1]
+    if len(answer) > 15:
+        answer = answer[0:15]
+        if answer.endswith('.'):
+            answer = answer[:-1]
+    if answer == '':
+        answer = 'a'
+    for _ in range(len(answer), 3):
+        answer += answer[-1]     
+    return answer
+    
+    
+
+print(solution('...!@BaT#*..y.abcdefghijklm'))
